d79126605b8e782cc68b4df0ea53e55272811869
marvin lara <laramarvin105@gmail.com>
2020-06-19 17:00:22 -0400

    added react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import './App.css';
+import ProductScreen from './Screens/productScreen';
+import HomeScreen from './Screens/homeScreen';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
-function App() {
+function App(props) {
 	const openMenu = () => {
 		document.querySelector('.sidebar').classList.add('open');
 	};
@@ -10,87 +13,45 @@ function App() {
 		document.querySelector('.sidebar').classList.remove('open');
 	};
 	return (
-		<div className="grid-container">
-			<header className="header">
-				<div className="brand">
-					<button onClick={openMenu}>&#9776;</button>
-					<a href="index.html">E-Commers</a>
-				</div>
-				<div className="header-links">
-					<a href="cart.html">Cart</a>
-					<a href="signin.html">Sign In</a>
-				</div>
-			</header>
-			<aside className="sidebar">
-				<h4>Shopping Categories</h4>
-				<button className="sidebar-close-button" onClick={closeMenu}>
-					x
-				</button>
-				<ul>
-					<li>
-						<a href="index.html">Pants</a>
-					</li>
-					<li>
-						<a href="index.html">Shirts</a>
-					</li>
-				</ul>
-			</aside>
-			<main className="main">
-				<div className="content">
-					<ul className="products">
+		<BrowserRouter>
+			<div className="grid-container">
+				<header className="header">
+					<div className="brand">
+						<button onClick={openMenu}>&#9776;</button>
+						<Link to="/"> E-Commers </Link>
+					</div>
+					<div className="header-links">
+						<a href="cart.html">Cart</a>
+						<a href="signin.html">Sign In</a>
+					</div>
+				</header>
+				<aside className="sidebar">
+					<h4>Shopping Categories</h4>
+					<button className="sidebar-close-button" onClick={closeMenu}>
+						x
+					</button>
+					<ul>
 						<li>
-							<div className="product">
-								<img
-									src="/images/d1.jpg"
-									alt="product"
-									className="product-image"
-								/>
-								<div className="product-name">
-									<a href="product.html">Slim Shirt</a>
-								</div>
-								<div className="product-brand">Nike</div>
-								<div className="product-price">$60</div>
-								<div className="product-rating">4.5 Stars (10) Reviews</div>
-							</div>
+							<a href="index.html">Pants</a>
 						</li>
 						<li>
-							<div class="product">
-								<img src="/images/d1.jpg" alt="product" class="product-image" />
-								<div class="product-name">
-									<a href="product.html">Slim Shirt</a>
-								</div>
-								<div class="product-brand">Nike</div>
-								<div class="product-price">$60</div>
-								<div class="product-rating">4.5 Stars (10) Reviews</div>
-							</div>
-						</li>
-						<li>
-							<div class="product">
-								<img src="/images/d1.jpg" alt="product" class="product-image" />
-								<div class="product-name">
-									<a href="product.html">Slim Shirt</a>
-								</div>
-								<div class="product-brand">Nike</div>
-								<div class="product-price">$60</div>
-								<div class="product-rating">4.5 Stars (10) Reviews</div>
-							</div>
-						</li>
-						<li>
-							<div class="product">
-								<img src="/images/d1.jpg" alt="product" class="product-image" />
-								<div class="product-name">
-									<a href="product.html">Slim Shirt</a>
-								</div>
-								<div class="product-brand">Nike</div>
-								<div class="product-price">$60</div>
-								<div class="product-rating">4.5 Stars (10) Reviews</div>
-							</div>
+							<a href="index.html">Shirts</a>
 						</li>
 					</ul>
-				</div>
-			</main>
-			<footer class="footer">All rights reserved.</footer>
-		</div>
+				</aside>
+				<main className="main">
+					<div className="content">
+						<Route
+							path="/products/:id"
+							exact={true}
+							component={ProductScreen}
+						/>
+						<Route path="/" exact={true} component={HomeScreen} />
+					</div>
+				</main>
+				<footer className="footer">All rights reserved.</footer>
+			</div>
+		</BrowserRouter>
 	);
 }
 
---
+++ client/src/Screens/productScreen.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import data from '../data';
+
+function ProductScreen(props) {
+	console.log('productscreen');
+	return <div>ProductScreen</div>;
+}
+
+export default ProductScreen;
