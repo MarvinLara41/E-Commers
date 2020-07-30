c9766e1cf5c6fed4e7ce8fe507bfe6b6a7f7bb72
marvin lara <laramarvin105@gmail.com>
2020-07-30 19:34:05 -0400

    update client

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-	JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
+	JWT_SECRET: process.env.JWT_SECRET,
 	PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID,
 	MONGODB_URI: process.env.MONGODB_URI,
 	PORT: process.env.PORT || 5000,
