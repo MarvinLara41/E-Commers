dd951a252ae3678e7ec4e0cce951a4766900f708
marvin lara <laramarvin105@gmail.com>
2020-07-30 20:00:37 -0400

    edit

--- config.js
+++ config.js
@@ -5,6 +5,4 @@ module.exports = {
 	JWT_SECRET: process.env.JWT_SECRET,
 	PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID,
 	MONGODB_URI: process.env.MONGODB_URI,
-	PORT: process.env.PORT || 5000,
-	HOST: process.env.HOST,
 };
